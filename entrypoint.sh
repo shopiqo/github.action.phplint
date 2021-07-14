7a4ac3757ffefa2eb757e737aa268799195f38db
Igor Ermentraut <i.ermentraut@webweit.de>
2021-07-14 18:30:21 +0200

    updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 
 set -xe
 
-/root/.composer/vendor/bin/phplint -vvv -c /root/.phplint.yml
+if [ -f /code/.phplint.yml ]; then
+  /root/.composer/vendor/bin/phplint -vvv -c /code/.phplint.yml
+else
+  /root/.composer/vendor/bin/phplint -vvv -c /root/.phplint.yml
+fi
